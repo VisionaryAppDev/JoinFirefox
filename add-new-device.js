5ed0a392843c5c9c2f5ffcb576db65b99735806e
Norin <ke_norin@yahoo.com>
2019-07-11 16:20:05 +0700

    able to add and remove data from local storage

--- add-new-device.js
+++ add-new-device.js
@@ -1,56 +1,83 @@
 $(function(){
-    $("#btnAddNewDevice").click(async function(){
-        let deviceName= $("#deviceName").val();
-        let deviceId= $("#deviceId").val();
-        let apiToken = $("#apiToken").val();
-        let pwd = $("#pwd").val();
-        // let id = Date.now().toString();
 
+    initDevices();
 
+    
+    async function initDevices(){
+        var devices = await getDevicesFromLocalStorage();
+        for (const [deviceId, device] of Object.entries(devices)) {
+            addNewDeviceToScreen(device);
+        }
+    }
+    
 
-        // read and write back to storage
-        var device = { deviceName: deviceName, deviceId: deviceId, apiToken: apiToken, pwd: pwd };
+    async function getDevicesFromLocalStorage() {
         var devices = await browser.storage.local.get("devices");
         devices = typeof devices.devices === 'undefined' ? {} : devices.devices; 
-        devices[deviceId] = device;
-        browser.storage.local.set({ devices: devices});
 
+        return devices;
+    }
+    
 
-        // Output to screen
-        $("#divExistingDevices").append(getNewAddedDeviceHtml(deviceId, deviceName, apiToken, pwd));
-
-
-
-        // browser.storage.sync.set({'key': 'val'});
-        // browser.storage.managed.set({'key': 'val'});
+    function addNewDeviceToScreen(device) {
+        $("#divExistingDevices").append(getNewAddedDeviceHtml(device));
+        removeDeviceEvent(device.id);
+    }
+    
+    
+    $("#btnAddNewDevice").click(async function(){
+        let deviceName = $("#deviceName").val();
+        let deviceId   = $("#deviceId").val();
+        let apiToken   = $("#apiToken").val();
+        let pwd        = $("#pwd").val();
 
         
-        // TEST output
-        var result = await browser.storage.local.get('devices');
-        console.log(result);
+        // read and write back to storage
+        var device = { name: deviceName, id: deviceId, apiToken: apiToken, pwd: pwd };
+        var devices = await getDevicesFromLocalStorage();
+        devices[device.id] = device;
+        browser.storage.local.set({ devices: devices});
+
+        
+        // browser.storage.sync.set({'key': 'val'});
+        // browser.storage.managed.set({'key': 'val'});
+        addNewDeviceToScreen(device);        
     })
 
 
-    function getNewAddedDeviceHtml(deviceId, deviceName, apiToken, pwd){
+    async function removeDeviceEvent(deviceId) {
+        $(`#btnDelDevice${deviceId}`).click(async function() {
+            $(`#deviceDivId${deviceId}`).remove();
+            
+            var devices = await getDevicesFromLocalStorage();
+            delete devices[deviceId];
+            browser.storage.local.set({ devices: devices});
+        });
+    }
+
+    
+    
+
+
+    function getNewAddedDeviceHtml(device){
         return `
-           <div class="container-fluid" id=id${deviceId}>
+           <div class="container-fluid" id="deviceDivId${device.id}">
                <div class="form-inline">
                    <label for="deviceName">Device Name:</label>
-                   <input type="text" class="form-control" id="deviceName${deviceId}" value="${deviceName}">
+                   <input type="text" class="form-control" id="deviceName${device.id}" value="${device.name}">
 
                    <label for="deviceName">Device Id:</label>
-                   <input type="text" class="form-control" id="deviceId${deviceId}" value="${deviceId}">
+                   <input type="text" class="form-control" id="deviceId${device.id}" value="${device.id}">
 
                    <label for="apiToken">Api token:</label>
-                   <input type="text" class="form-control" id="apiToken${deviceId}" value=${apiToken}>
+                   <input type="text" class="form-control" id="apiToken${device.id}" value=${device.apiToken}>
 
                    <label for="pwd">Password:</label>
-                   <input type="password" class="form-control" id="pwd${deviceId}" value=${pwd}>
+                   <input type="password" class="form-control" id="pwd${device.id}" value=${device.pwd}>
 
                    <button type="button" class="btn btn-primary">Save</button>
-                   <button type="button" class="btn btn-danger">Delete</button>
+                   <button type="button" class="btn btn-danger" id="btnDelDevice${device.id}">Delete</button>
                </div>
            </div>`;
-
     }
 })
