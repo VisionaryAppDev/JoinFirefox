7d980c1a267e68cc5fb94bfd6be06cd9fc3384eb
Norin <ke_norin@yahoo.com>
2019-07-11 15:09:35 +0700

    able to read and write as object to local storage

--- add-new-device.js
+++ add-new-device.js
@@ -7,22 +7,26 @@ $(function(){
         // let id = Date.now().toString();
 
 
-        console.log(deviceId, deviceName, apiToken, pwd);
+
+        // read and write back to storage
+        var device = { deviceName: deviceName, deviceId: deviceId, apiToken: apiToken, pwd: pwd };
+        var devices = await browser.storage.local.get("devices");
+        devices = typeof devices.devices === 'undefined' ? {} : devices.devices; 
+        devices[deviceId] = device;
+        browser.storage.local.set({ devices: devices});
+
+
+        // Output to screen
         $("#divExistingDevices").append(getNewAddedDeviceHtml(deviceId, deviceName, apiToken, pwd));
 
 
 
-        
-        browser.storage.local.set(`{
-            ${id}: {
-                "deviceName": deviceName,
-                "apiToken": apiToken,
-                "pwd": pwd
-            }
-        }`);
         // browser.storage.sync.set({'key': 'val'});
         // browser.storage.managed.set({'key': 'val'});
-        var result = await browser.storage.local.get(deviceId);  
+
+        
+        // TEST output
+        var result = await browser.storage.local.get('devices');
         console.log(result);
     })
 
@@ -35,14 +39,14 @@ $(function(){
                    <input type="text" class="form-control" id="deviceName${deviceId}" value="${deviceName}">
 
                    <label for="deviceName">Device Id:</label>
-                   <input type="text" class="form-control" id="deviceId${deviceId}" value="${deviceId}">                 
- 
+                   <input type="text" class="form-control" id="deviceId${deviceId}" value="${deviceId}">
+
                    <label for="apiToken">Api token:</label>
                    <input type="text" class="form-control" id="apiToken${deviceId}" value=${apiToken}>
-                   
+
                    <label for="pwd">Password:</label>
                    <input type="password" class="form-control" id="pwd${deviceId}" value=${pwd}>
-                   
+
                    <button type="button" class="btn btn-primary">Save</button>
                    <button type="button" class="btn btn-danger">Delete</button>
                </div>
