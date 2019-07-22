0655c105dc3c822a9e746fbe2bf2abd7b2c7ebf8
Norin <ke_norin@yahoo.com>
2019-07-22 11:18:17 +0700

    get sample code for sub menu

--- add-new-device.js
+++ add-new-device.js
@@ -1,92 +1,92 @@
-$(function(){
+$(function () {
 
     initDevices();
 
-    
-    async function initDevices(){
+
+    async function initDevices() {
         var devices = await getDevicesFromLocalStorage();
         for (const [deviceId, device] of Object.entries(devices)) {
             addNewDeviceToScreen(device);
         }
     }
-    
+
 
     async function getDevicesFromLocalStorage() {
         var devices = await browser.storage.local.get("devices");
-        devices = typeof devices.devices === 'undefined' ? {} : devices.devices; 
+        devices = typeof devices.devices === 'undefined' ? {} : devices.devices;
 
         return devices;
     }
-    
+
 
     function addNewDeviceToScreen(device) {
         $("#divExistingDevices").append(getNewAddedDeviceHtml(device));
         removeDeviceEvent(device.id);
         updateDeviceEvent(device.id);
     }
-    
-    
-    $("#btnAddNewDevice").click(async function(){
-        let deviceName = $("#deviceName").val();
-        let deviceId   = $("#deviceId").val();
-        let apiToken   = $("#apiToken").val();
-        let pwd        = $("#pwd").val();
 
-        
-        if(deviceId === "" ) return;
-        
-        
+
+    $("#btnAddNewDevice").click(async function () {
+        let deviceName = $("#deviceName").val();
+        let deviceId = $("#deviceId").val();
+        let apiToken = $("#apiToken").val();
+        let pwd = $("#pwd").val();
+
+
+        if (deviceId === "") return;
+
+
         // read and write back to storage
         var device = { name: deviceName, id: deviceId, apiToken: apiToken, pwd: pwd };
         var devices = await getDevicesFromLocalStorage();
         devices[device.id] = device;
-        browser.storage.local.set({ devices: devices});
+        browser.storage.local.set({ devices: devices });
 
-        
+
         // browser.storage.sync.set({'key': 'val'});
         // browser.storage.managed.set({'key': 'val'});
-        addNewDeviceToScreen(device);        
+        addNewDeviceToScreen(device);
     })
 
 
     async function removeDeviceEvent(deviceId) {
-        $(`#btnDelDevice${deviceId}`).click(async function() {
+        $(`#btnDelDevice${deviceId}`).click(async function () {
             $(`#deviceDivId${deviceId}`).remove();
-            
+
             var devices = await getDevicesFromLocalStorage();
             delete devices[deviceId];
-            browser.storage.local.set({ devices: devices});
+            browser.storage.local.set({ devices: devices });
         });
     }
 
     async function updateDeviceEvent(deviceId) {
-        $(`#btnUpdateDevice${deviceId}`).click(async function(){
+        $(`#btnUpdateDevice${deviceId}`).click(async function () {
             // alert(deviceId);
-            if(deviceId === "" ) return;
+            if (deviceId === "") return;
 
             // let newDeviceId = $(`#deviceId${deviceId}`).val();
             let deviceName = $(`#deviceName${deviceId}`).val();
-            let apiToken   = $(`#apiToken${deviceId}`).val();
-            let pwd        = $(`#pwd${deviceId}`).val();
+            let apiToken = $(`#apiToken${deviceId}`).val();
+            let pwd = $(`#pwd${deviceId}`).val();
 
             // TODO:
             // allow to update device id,
             // set deviceId = new device id from form
             // delete old data because add new device id will create another record
-            // loop through cmds and update 
-            
+            // loop through cmds and update
+
             var device = { name: deviceName, id: deviceId, apiToken: apiToken, pwd: pwd };
             var devices = await getDevicesFromLocalStorage();
             devices[device.id] = device;
-            browser.storage.local.set({ devices: devices});
+            browser.storage.local.set({ devices: devices });
         });
     }
 
-    
-    
 
 
-    function getNewAddedDeviceHtml(device){
+
+
+    function getNewAddedDeviceHtml(device) {
         return `
            <div class="container-fluid" id="deviceDivId${device.id}">
                <div class="form-inline">
@@ -125,16 +125,16 @@ $(function(){
 
     initCmds();
 
-    
-    async function initCmds(){
-        // 
+
+    async function initCmds() {
+        //
         // $("#cmdDeviceId option").remove();
         var devices = await getDevicesFromLocalStorage();
         for (const [deviceId, device] of Object.entries(devices)) {
             $("#cmdDeviceId").append(new Option(device.name, device.id));
         }
 
-        
+
         //
         var cmds = await getCmdsFromLocalStorage();
         for (const [cmdId, cmd] of Object.entries(cmds)) {
@@ -148,63 +148,63 @@ $(function(){
         removeCmdEvent(cmd.id);
         updateCmdEvent(cmd.id);
     }
-    
 
-    $("#btnAddNewCmd").click(async function(){
+
+    $("#btnAddNewCmd").click(async function () {
         let deviceId = $("#cmdDeviceId").val();
-        let cmdId   = Date.now().toString();
+        let cmdId = Date.now().toString();
         let cmdName = $("#cmdName").val();
-        let cmdText   = $("#cmd").val();
+        let cmdText = $("#cmd").val();
 
-        
-        
+
+
         // read and write back to storage
         var cmd = { id: cmdId, name: cmdName, cmd: cmdText, deviceId: deviceId };
         var cmds = await getCmdsFromLocalStorage();
         cmds[cmd.id] = cmd;
-        browser.storage.local.set({ cmds: cmds});
+        browser.storage.local.set({ cmds: cmds });
 
-        
+
         // browser.storage.sync.set({'key': 'val'});
         // browser.storage.managed.set({'key': 'val'});
-        addNewCmdToScreen(cmd);        
+        addNewCmdToScreen(cmd);
     })
 
     async function getCmdsFromLocalStorage() {
         var cmds = await browser.storage.local.get("cmds");
-        cmds = typeof cmds.cmds === 'undefined' ? {} : cmds.cmds; 
+        cmds = typeof cmds.cmds === 'undefined' ? {} : cmds.cmds;
 
         return cmds;
     }
 
 
     async function removeCmdEvent(cmdId) {
-        $(`#btnDelCmd${cmdId}`).click(async function() {
+        $(`#btnDelCmd${cmdId}`).click(async function () {
             $(`#cmdDivId${cmdId}`).remove();
-            
+
             var cmds = await getCmdsFromLocalStorage();
             delete cmds[cmdId];
-            browser.storage.local.set({ cmds: cmds});
+            browser.storage.local.set({ cmds: cmds });
         });
     }
 
-     async function updateCmdEvent(cmdId) {
-        $(`#btnSaveCmd${cmdId}`).click(async function() {
+    async function updateCmdEvent(cmdId) {
+        $(`#btnSaveCmd${cmdId}`).click(async function () {
             let deviceId = $(`#cmdDeviceId${cmdId}`).val();
             let cmdName = $(`#cmdName${cmdId}`).val();
-            let cmdText   = $(`#cmd${cmdId}`).val();
+            let cmdText = $(`#cmd${cmdId}`).val();
 
-        
-        
+
+
             // read and write back to storage
             var cmd = { id: cmdId, name: cmdName, cmd: cmdText, deviceId: deviceId };
             var cmds = await getCmdsFromLocalStorage();
             cmds[cmd.id] = cmd;
-            browser.storage.local.set({ cmds: cmds});
+            browser.storage.local.set({ cmds: cmds });
         });
     }
 
-    
+
 
 
     async function getNewAddedCmdHtml(cmd) {
@@ -213,13 +213,13 @@ $(function(){
              <div class="form-inline" id="cmdDivId${cmd.id}">
                  <label for="deviceName">Device Name:</label>
                  <select class="custom-select custom-select-mb" id="cmdDeviceId${cmd.id}">
-                     ${Object.entries(devices).map(device=>
-                       `<option value="${device[1].id}" ${cmd.deviceId == device[1].id ? "selected" : ""}>
+                     ${Object.entries(devices).map(device =>
+            `<option value="${device[1].id}" ${cmd.deviceId == device[1].id ? "selected" : ""}>
                             ${device[1].name}
                         </option>
-                     `)}                   
+                     `)}
                  </select>
-                 
+
                  <label for="apiToken">Command Name:</label>
                  <input type="text" class="form-control" id="cmdName${cmd.id}" value="${cmd.name}">
                  <label for="cmd">Command:</label>
@@ -232,23 +232,23 @@ $(function(){
 
 
     initTestDevices();
-    
-    async function initTestDevices(){
+
+    async function initTestDevices() {
         var devices = await getDevicesFromLocalStorage();
-        Object.entries(devices).forEach((device, ind)=> {
+        Object.entries(devices).forEach((device, ind) => {
             var option = new Option(device[1].name, `testDevice${device[1].id}`);
             $("#testDevices").append(option);
             // option.data(device);
             option.setAttribute('data-id', device[1])
         });
-       
+
         console.log("")
-    } 
+    }
 
     sendTestCmdEvent();
-    
-    async function sendTestCmdEvent(){
-        $("#btnSendTestCmd").click(async function(){
+
+    async function sendTestCmdEvent() {
+        $("#btnSendTestCmd").click(async function () {
             var testCmd = $("#testCmd").val().trim();
             var selectedDevice = $("#testDevices").val();
             var device = {};
@@ -256,8 +256,7 @@ $(function(){
 
             var devices = await getDevicesFromLocalStorage();
             for (const [deviceId, device_] of Object.entries(devices)) {
-                if(selectedDevice == `testDevice${device_.id}`)
-                {
+                if (selectedDevice == `testDevice${device_.id}`) {
                     device = device_;
                 }
             }
@@ -265,10 +264,10 @@ $(function(){
             var pwd = device.pwd != "" ? `&password=${device.pwd}` : "";
             var cmd = `${device.apiToken}&${testCmd}${pwd}`;
             fetch(cmd)
-                .then(function(response) {
+                .then(function (response) {
                     return response;
                 })
-                .then(function(myJson) {
+                .then(function (myJson) {
                     console.log((myJson));
                 });
         })
@@ -278,45 +277,82 @@ $(function(){
 
 
     //// Context menu
-    function onCreated() {
+    async function onCreated() {
+        var currentUrl = await browser.tabs.getCurrent().url;
+        console.log(await browser.tabs.getCurrent());
+
         if (browser.runtime.lastError) {
             console.log(`Error: ${browser.runtime.lastError}`);
         } else {
             console.log("Item created successfully");
+            
         }
     }
-    
-    browser.menus.create({
-        id: "remove-me",
-        title: "test title",
-        contexts: ["all"]
-    }, onCreated);
-    
+    initDevicesMenu();
+    initCmdsMenu();
+
+
+    async function initDevicesMenu() {
+        var devices = await getDevicesFromLocalStorage();
+        for (const [deviceId, device] of Object.entries(devices)) {
+            browser.menus.create({
+                id: `device${device.id}`,
+                title: device.name,
+                contexts: ["all"],
+            }, onCreated);
+
+            // clipboard: -text. to phone clip
+
+            // link: -text, -open link
+
+            // selected text: -
+
+            // current page: -text, -   link
+
+            // download youtube
+
+            //
+        }
+    }
+
+    async function initCmdsMenu() {
+        var cmds = await getCmdsFromLocalStorage();
+        for (const [cmdId, cmd] of Object.entries(cmds)) {
+            browser.menus.create({
+                id: `cmd${cmd.id}`,
+                parentId: `device${cmd.deviceId}`,
+                title: `cmd${cmd.name}`,
+                contexts: ["all"],
+            }, onCreated);
+        }
+    }
+
+
     browser.menus.create({
         id: "separator1-1",
         type: "separator",
         contexts: ["all"]
     }, onCreated);
-    
-    browser.menus.create({
-        id: "remove1-me",
-        title: "test title",
-        contexts: ["all"]
-    }, onCreated);
 
-  browser.contextMenus.create({
-    id: "copy-link-to-clipboard",
-    title: "Copy link to clipboard",
-    contexts: ["link"],
-});
+
+
+    browser.contextMenus.create({
+        id: "copy-link-to-clipboard",
+        title: "Copy link to clipboard",
+        contexts: ["link"],
+    });
 
 
     browser.menus.onClicked.addListener((info, tab) => {
         switch (info.menuItemId) {
-        case "remove-me":
-            var removing = browser.menus.remove(info.menuItemId);
-            removing.then(onRemoved, onError);
-            break;
+            case "remove-me":
+                var removing = browser.menus.remove(info.menuItemId);
+                removing.then(onRemoved, onError);
+                break;
         }
+        console.log(tab);
+        console.log(info);
+
+        // browser.tabs.getCurrent();
     });
 })
